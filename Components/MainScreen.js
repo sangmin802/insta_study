6af960ae0dd6c3e65eeaf5e40b321288bb87fc34
sangmin802 <56113148+sangmin802@users.noreply.github.com>
2020-03-16 16:35:08 +0900

    2day

--- Components/MainScreen.js
+++ Components/MainScreen.js
@@ -1,16 +1,59 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+import HomeTab from './AppTabNavigator/HomeTab.js'
+import SearchTab from './AppTabNavigator/SearchTab.js'
+import AddMediaTab from './AppTabNavigator/AddMediaTab.js'
+import LikesTab from './AppTabNavigator/LikesTab.js'
+import ProfileTab from './AppTabNavigator/ProfileTab.js'
 
 export default class MainScreen extends React.Component {
   render(){
-    console.log(this.props)
     return (
-      <View style={styles.container}>
-        <Text>MainScreen</Text>
-      </View>
+      <Tab.Navigator
+        screenOptions={({ route : { name } }) => ({
+          tabBarIcon : ({ focused, color, size }) => {
+            let iconName;
+            if(name === 'Home'){
+              iconName = 'ios-home';
+            }else if(name === 'Search'){
+              iconName = 'ios-search';
+            }else if(name === 'AddMedia'){
+              iconName = 'ios-add-circle';
+            }else if(name === 'Likes'){
+              iconName = 'ios-heart';
+            }else if(name === 'Profile'){
+              iconName = 'ios-person';
+            };
+            return <Ionicons name={iconName} size={26} color={color} />;
+          }
+        })}
+        tabBarOptions={{
+          activeTintColor : '#000',
+          inactiveTintColor : '#d1cece',
+          showLabel : false,
+          showIcon : true,
+          indicatorStyle : {
+            width : 0
+          },
+        }}
+        tabBarPosition='bottom'
+        backBehavior='history'
+      >
+        <Tab.Screen name="Home" component={HomeTab}/>
+        <Tab.Screen name="Search" component={SearchTab} />
+        <Tab.Screen name="AddMedia" component={AddMediaTab} />
+        <Tab.Screen name="Likes" component={LikesTab} />
+        <Tab.Screen name="Profile" component={ProfileTab} />
+      </Tab.Navigator>
     );
   }
 }
+
+const Tab = createMaterialTopTabNavigator();
 
 const styles = StyleSheet.create({
   container: {
--- README.md
+++ README.md
@@ -10,4 +10,8 @@ Google velog로 올려진 React Native Instagram은 구버전이라서, Navigati
   4. NavigationContainer는 기존 Router태그와 같은 용도같음
   5. const Stack = createStackNavigator() 로 Stack 객체를 받아옴
   6. Stack.Navigator 안에, Stack.Screen으로 컴포넌트를 연동시켜줌.
-  7. AppTabNavigator해야함. 근데 BottomTab으로 바뀐듯함. 모듈은 깔았음.
+  7. AppTabNavigator.  
+    + AppTabNavigator(createBottomTabNavigator사용)는 NavigationContainer 안에 있어야 함. 마치 WEB의 Route과 비슷한듯.  
+    + Tab.Navigator는 여러가지 props를 보낼 수 있는데, screenOptions로 보내지는것을 보면, 객체를 보내며, 그 객체에는 현재 출력되고 있는 component의 설정된 name이 있음. 그것을 활용하여, 어떤것이 클릭되었는지 체크 할 수 있음. (pathname과 같은역활)  
+    + TabBarIcon은 Tab.screen의 속성이지만, 유지보수를 위해 Tab.Navigator의 screenOptions의 속성으로도 설정할 수 있다.  
+  8. 탭을 클릭했을 때, swipe기능을 사용하려면, createMaterialTopTabNavigator를 사용해야한다. const로 호출되는 메소드명이 바뀌고, 새로운 속성들을 사용할 수 있다.(showIcon 기본 true였는데, 바꾸고 입력필수항목이 되었음)
